4b7e23a4f16d8b5b5ec6ecc0c85d49ea749ede74
johnanderluh <john.anderluh@student.tamdistrict.org>
2024-10-29 11:26:04 -0700

    Final clean

--- ChoseYourPath.java
+++ ChoseYourPath.java
@@ -55,63 +55,74 @@ public class ChoseYourPath {
       System.out.println("You dodge. The " + userInput2 + " " + userInput3 + " is very sad. Look what you did.");
       System.out.println("Do you comfort the " + userInput2 + " " + userInput3 + " or do you gloat? (comfort/gloat)");
       answer = sc.nextLine();
-    }
-    // dodge, comfort
-    if (answer.equals("comfort")) {
-      System.out.println(
-          "You comfort the " + userInput2 + " " + userInput3 + ". Suddenly, Aaron Rodgers knocks on the door.");
-      System.out.println("Do you open it? (yes/no)");
-      answer = sc.nextLine();
-    }
-    // dodge, comfort, open door
-    if (answer.equals("yes")) {
-      System.out.println(
-          "You open the door. Its Aaron Rodgers! \"Hi\" he says. \"I am signing you to the Jets!\" You live happily ever after.");
-    }
-    // dodge, comfort, don't open door
-    else if (answer.equals("no")) {
-      System.out.println("You don't open the door. Suddenly, the " + userInput2 + " " + userInput3
-          + " runs toward the door. Aaron rodgers is it's favorite quarterback. You are in the way and are trampled and die.");
-    }
-    // dodge, gloat
-    else if (answer.equals("gloat")) {
-      System.out.println("You gloat in front of the " + userInput2 + " " + userInput3 + ". It begins to cry.");
-      System.out.println("Do you help it? (yes/no)");
-      answer = sc.nextLine();
-    }
-    // dodge, gloat, help
-    if (answer.equals("yes")) {
-      System.out.println("You help the " + userInput2 + " " + userInput3
-          + ". It agrees to become friends and you live happily ever after.");
-    }
-    // dodge, gloat, don't help
-    else if (answer.equals("no")) {
-      System.out.println("You don't help the " + userInput2 + " " + userInput3
-          + ". Suddenly, a camera crew comes out. It was a social experiment and you failed! You die of shame.");
+
+      // dodge, comfort
+      if (answer.equals("comfort")) {
+        System.out.println(
+            "You comfort the " + userInput2 + " " + userInput3 + ". Suddenly, Aaron Rodgers knocks on the door.");
+        System.out.println("Do you open it? (yes/no)");
+        answer = sc.nextLine();
+
+        // dodge, comfort, open door
+        if (answer.equals("yes")) {
+          System.out.println(
+              "You open the door. Its Aaron Rodgers! \"Hi\" he says. \"I am signing you to the Jets!\" You live happily ever after.");
+        }
+
+        // dodge, comfort, don't open door
+        else if (answer.equals("no")) {
+          System.out.println("You don't open the door. Suddenly, the " + userInput2 + " " + userInput3
+              + " runs toward the door. Aaron rodgers is it's favorite quarterback. You are in the way and are trampled and die.");
+        }
+      }
+      // dodge, gloat
+      else if (answer.equals("gloat")) {
+        System.out.println("You gloat in front of the " + userInput2 + " " + userInput3 + ". It begins to cry.");
+        System.out.println("Do you help it? (yes/no)");
+        answer = sc.nextLine();
+
+        // dodge, gloat, help
+        if (answer.equals("yes")) {
+          System.out.println("You help the " + userInput2 + " " + userInput3
+              + ". It agrees to become friends and you live happily ever after.");
+        }
+        // dodge, gloat, don't help
+        else if (answer.equals("no")) {
+          System.out.println("You don't help the " + userInput2 + " " + userInput3
+              + ". Suddenly, a camera crew comes out. It was a social experiment and you failed! You die of shame.");
+        }
+      }
     }
     // don't dodge
     else if (answer.equals("no")) {
       System.out.println("You stand still. The " + userInput2 + " " + userInput3
           + " runs up to you and gives you a hug. You are now friends");
-      System.out.println("The " + userInput2 + " " + userInput3 + "offers you coffee. Do you drink it? (yes/no)");
+      System.out.println("The " + userInput2 + " " + userInput3 + " offers you coffee. Do you drink it? (yes/no)");
       answer = sc.nextLine();
-    }
-    // don't dodge, drink coffee
-    if (answer.equals("yes")) {
-      System.out.println("The coffee is poisoned. You die");
-    }
-    // don't dodge, don't drink coffee
-    if (answer.equals("no")) {
-      System.out.println("You decline the coffee. The " + userInput2 + " " + userInput3 + "is very sad.");
-      System.out.println("Do you comfort it or do you to erase the mad libs? (comfort/erase)");
-      answer = sc.nextLine();
-    }
-    if (answer.equals("comfort")) {
-      System.out
-          .println("You become freinds with the " + userInput2 + " " + userInput3 + ". You live happily ever after.");
-    } else if (answer.equals("no")) {
-      System.out.println("You lunge for the mad libs, trying to erase them. The " + userInput2 + " " + userInput3
-          + "grabs your hand. \"Naugty naugty\". The end");
+
+      // don't dodge, drink coffee
+      if (answer.equals("yes")) {
+        System.out.println("The coffee is poisoned. You die");
+      }
+      // don't dodge, don't drink coffee
+      else if (answer.equals("no")) {
+        System.out.println("You decline the coffee. The " + userInput2 + " " + userInput3 + " is very sad.");
+        System.out.println("Do you comfort it or do you try to erase the mad libs? (comfort/erase)");
+        answer = sc.nextLine();
+
+        // don't dodge, don't drink coffee, comfort
+        if (answer.equals("comfort")) {
+          System.out
+              .println(
+                  "You comfort the " + userInput2 + " " + userInput3
+                      + " and become friends. You live happily ever after.");
+        }
+        // don't dodge, don't drink coffee, erase
+        else if (answer.equals("erase")) {
+          System.out.println("You lunge for the mad libs, trying to erase them. The " + userInput2 + " " + userInput3
+              + " grabs your hand. \"Naugty naugty\". The end.");
+        }
+      }
     }
   }
 }
